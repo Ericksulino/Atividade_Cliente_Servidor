0ba49fa11c66baf6e33a91593e2ec318e2a417e7
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-15 20:32:43 -0300

    adicionando funcionaliade de sair

--- cliente.py
+++ cliente.py
@@ -4,7 +4,8 @@ port = 8001
 addr = ((ip,port))
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect(addr)
-while(True):
+mensagem = ''
+while(mensagem!= 'sair'):
     mensagem = input('digite uma mensagem para enviar ao servidor: ')
     client_socket.send(mensagem.encode())
     #print('mensagem enviada')
